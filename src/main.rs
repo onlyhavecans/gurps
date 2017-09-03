eec017eaf7b4e8a8d098a7f06615000546ace078
David Aronsohn <WagThatTail@Me.com>
2017-09-03 10:15:48 +0200

    All my features

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,13 @@ fn quick_roll(){
     println!("Total: {}", r);
 }
 
-fn roll_against(s: &str){
+fn roll_against(i: std::str::SplitWhitespace){
+    let mut mi = i;
+    let s = match mi.next() {
+        Some(s) => s,
+        _ => "10"
+    };
+
     let n: i32 = match s.parse::<i32>() {
         Ok(n) => n,
         Err(_) => 0,
@@ -44,14 +50,23 @@ fn roll_against(s: &str){
     if n == 0 || n > 18 {
         println!("Rolling against {} is an error", n);
         return
-    }
+    };
 
     let r: i32 = roll_me(GRUPS_ROLL);
-    match n.cmp(&r) {
+    let win: bool = match r.cmp(&n) {
         Ordering::Less => true,
         Ordering::Equal => true,
         Ordering::Greater => false
     };
+
+    let delta: i32 = if win {
+        print!("Success!");
+        n - r
+    } else {
+        print!("Failure!");
+        r - n
+    };
+    println!(" delta {}", delta);
 }
 
 fn main() {
@@ -63,7 +78,7 @@ fn main() {
             Some("q") => break,
             Some("h") => help_me(),
             Some("r") => quick_roll(),
-            Some("ra") => roll_against(&iter.next().unwrap()),
+            Some("ra") => roll_against(iter),
             _ => continue,
         };
     }
